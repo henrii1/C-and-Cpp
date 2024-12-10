abbb6085d4b751d7d359da931158fdddbc79ff57
henry emerald <emerald.henry@stu.cu.edu.ng>
2024-12-10 16:53:19 +0100

    updates

--- llm/train_llm.c
+++ llm/train_llm.c
@@ -44,7 +44,7 @@ void encoder_forward(float* out, int* inp, float* wte, float* wpe,
                                     //seek to the position of wte corresponding to token t
                                     float* wte_ix = wte + ix * C;
                                     //seek to the position of wpe corresponding to position
-                                    float* wpe_ix = wpe + t * C;
+                                    float* wpe_t = wpe + t * C;
                                     // add the two vectors and store result
                                     for (int i=0; i<C; i++){
                                         out_bt[i] = wte_ix[i] + wpe_t[i]
@@ -157,3 +157,248 @@ void layernorm_backward(float* dinp, float*dweight, float*dbias, float* dout, fl
                                 }
                             }
                         }
+
+
+void matmul_forward_naive(float* out, const float* inp, const float* weight,
+                            const float* bias, int B, int T, int C, int OC){
+                                //naive implementation of matrix multiplication
+                                #pragma omp parallel for collapse(2) // for parallelizing matrix multiplication
+                                for (int b=0; b<B; b++){
+                                    for (int t=0; t<T; t++){
+                                        //seek to position
+                                        int bt = b * T + t;
+                                        for (int o=0; o<OC; o++){
+                                            float val = (bias != NULL) ? bias[o] : 0.0f;
+                                            for (int i = 0; i<C; i++){
+                                                val += inp[bt * C + i] * weight[o*C + i];
+                                            }
+                                            out[bt * OC + o] = val;
+                                        }
+
+                                    }
+                                }
+                            }
+
+
+void matmul_forward(float* out, const float* inp, const float* weight, const float* bias,
+                    int B, int T, int C, int OC){
+                        //mildly optimized implementation of matmul forward
+                        
+                        //default to naive if modulus isn't zero
+                        const int LOOP_UNROLL = 8;
+                        if (B*T % LOOP_UNROLL != 0){
+                            matmul_forward_naive(out, inp, weight, bias, B, T, C, OC);
+                            return;
+                        }
+
+                        //convert B and T into one strided loop
+                        #pragma omp parallel for  //parellizes loops
+                        for (int obt=0; obt<B*T; obt += LOOP_UNROLL){
+                            for (int o=0; o<OC; o++){
+                                float result[LOOP_UNROLL];
+
+                                //initialize bias
+                                for (int ibt=0; ibt<LOOP_UNROLL; ibt++){
+                                    result[ibt] = (bias != NULL) ? bias[o] : 0.0f;
+                                }
+
+                                //a bit of caching
+                                for (int i=0; i<C; i++){
+                                    float w = weight[i + o * C];
+                                    for (int ibt=0; ibt<LOOP_UNROLL; ibt++){
+                                        int bt = obt + ibt;
+                                        result[ibt] += inp[bt * C + i] * w;
+                                    }
+                                }
+                                // write back results to main memory
+                                for (int ibt=0; ibt<LOOP_UNROLL; ibt++){
+                                    int bt = obt + ibt;
+                                    out[bt*OC + o] = result[ibt];
+                                }
+                            }
+                        }
+
+                    }
+
+
+
+void matmul_backward(float* dinp, float* dweight, float* dbias,
+                        const float* dout, const float* inp, const float* weight,
+                        int B, int T, int C, int OC){
+
+                            //backward into inp first
+                            #pragma omp parallel for collapse(2)
+                            for (int b=0; b<B; b++){
+                                for (int t=0; t<T; t++){
+                                    const float* dout_bt = dout + b * T * OC + t * OC;
+                                    float* dinp_bt = dinp + b * T * C + t * C;
+                                    for (int o=0; o<OC; o++){
+                                        const float* wrow = weight + o*C;
+                                        float d = dout_bt[o];
+                                        for (int i=0; i<C; i++){
+                                        dinp_bt[i] += wrow[i] * d;
+                                        }
+                                    }
+                                }
+                            }
+                            //backward into weight/bias, parellized over output channels OC
+                            #pragma omp parallel for
+                            for (int o=0; o<OC; o++){
+                                for (int b=0; b<B; b++){
+                                    for (int t=0; t<T; t++){
+                                        const float* dout_bt = dout + b*T*OC + t*OC;
+                                        const float* inp_bt = inp + b * T * C + t * C;
+                                        float* dwrow = dweight + o*C;
+                                        float d = dout_bt[o];
+                                        if (dbias != NULL) {dbias[o] += d; }
+                                        for (int i=0; i<C; i++){
+                                            dwrow[i] += inp_bt[i] * d;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
+void attention_forward(float* out, float* preatt, float* att, float* inp,
+                       int B, int T, int C, int NH){
+                        // input is (B, T, 3C) holding the query, key, value (Q, K, V) vectors
+                        // preatt, att are (B, NH, T, T). NH = number of heads, T = sequence length
+                        // that holds the pre-attention and post-attention scores (used in backward)
+                        // output is (B, T, C)
+                        // attention is the only layer that mixes information across time
+                        // every other operation is applied at every (b,t) position independently
+                        // (and of course, no layer mixes information across batch)
+                        int C3 = C*3;
+                        int hs = C/NH; // head size
+                        float scale = 1.0/sqrtf(hs);
+
+                        #pragma omp parallel for collapse(3)  //run parallel for 3 nested loops 
+                        for (int b=0; b<B; b++){
+                            for (int t=0; t<T; t++){
+                                for (int h=0; h<NH; h++){
+                                    //seek to positions
+                                    float* query_t = inp + b*T*C3 + t*C3 + h*hs;
+                                    float* preatt_bth = preatt + b*NH*T*T + h*T*T + t*T;
+                                    float* att_bth = att + b*NH*T*T + h*T*T + t*T;
+
+                                    //calculate query dot key and maxval
+                                    float maxval = -10000.0f;
+                                    for (int t2=0; t2<=t; t2++){
+                                        float* key_t2 = inp + b*T*C3 + t2*C3 + h*hs + C;
+
+                                        // dot product
+                                        float val = 0.0f;
+                                        for (int i=0; i<hs; i++){
+                                            val += query_t[i]* key_t2[i];
+                                        }
+                                        val *= scale;
+                                        if (val > maxval) {maxval = val;}
+                                        preatt_bth[t2] = val;
+
+                                    }
+                                    //calculate esp and keep track of sum
+                                    float expsum = 0.0f;
+                                    for (int t2=0; t2<=t; t2++){
+                                        float expv = expf(preatt_bth[t2] - maxval);
+                                        expsum += expv;
+                                        att_bth[t2] = expv;
+                                    }
+
+                                    float expsum_inv = expsum == 0.0f ? 0.0f : 1.0f/expsum;
+
+                                    //normalize to get the softmax
+                                    for(int t2=0; t2<T; t2++){
+                                        if (t2 <= t){
+                                            att_bth[t2] *= expsum_inv;
+                                        } else {
+                                            att_bth[t2] = 0.0f;
+                                        }
+                                    }
+
+                                    // accumulate weighted values into the output of attention
+                                    float* out_bth = out + b * T * C + t * C + h * hs;
+                                    for (int i=0; i<hs; i++) {out_bth[i] = 0.0f; }
+                                    for (int t2=0; t2<t; t2++){
+                                        float* value_t2 = inp + b*T*C3 + t2*C3 + h*hs + C*2;
+                                        float att_btht2 = att_bth[t2];
+                                        for (int i=0; i<hs; i++){
+                                            out_bth[i] += att_btht2*value_t2[i];
+                                        }
+                                    }
+
+
+                                }
+                            }
+                        }
+
+                       }
+
+
+void attention_backward(float* dinp, float* dpreatt, float* datt, float* dout,
+                        float* inp, float* att, int B, int T, int C, int NH){
+                             // inp/dinp are (B, T, 3C) Q,K,V
+                            // att/datt/dpreatt are (B, NH, T, T)
+                            // dout is (B, T, C)
+                            int C3 = C*3;
+                            int hs = C/NH; //head size
+                            float scale = 1.0f/sqrtf(hs);
+
+                            for (int b=0; b<B; b++){
+                                for (int t=0; t<T; t++){
+                                    for (int h=0; h<NH; h++){
+                                        float* att_bth = att + b*NH*T*T + h*T*T + t*T;
+                                        float* datt_bth = datt + b*NH*T*T + h*T*T + t*T;
+                                        float* dpreatt_bth = dpreatt + b*NH*T*T + h*T*T + t*T;
+                                        float* dquery_t = dinp + b*T*C3 + t*C3 + h*hs;
+                                        float* query_t = inp + b*T*C3 + t*C3 + h*hs;
+
+                                        //backward pass 4, through the value accumulation
+                                        float* dout_bth = dout + b*T*C + t*C + h*hs;
+                                        for (int t2=0; t2<=t; t2++){
+                                            float* value_t2 = inp + b*T*C3 + t2*C3 + h*hs + C*2;
+                                            float* dvalue_t2 = dinp + b*T*C3 + t2 * C3 + h*hs + C*2;
+                                            for (int i=0; i<hs; i++){
+                                                datt_bth[t2] += value_t2[i]*dout_bth[i];
+                                                dvalue_t2[i] += att_bth[t2]*dout_bth[i];
+                                            }
+                                        }
+                                        //backward pass 2&3, then softmax
+                                        for (int t2=0; t2<=t; t2++){
+                                            for (int t3=0; t3<=t; t3++){
+                                                float indicator = t2 == t3 ? 1.0f : 0.0f;
+                                                float local_derivative = att_bth[t2] * (indicator - att_bth[t3]);
+                                                dpreatt_bth[t3] += local_derivative*datt_bth[t2];
+                                            }
+                                        }
+
+                                        //backward pass 1, the q@k matmul
+                                        for (int t2=0; t2<=t; t2++){
+                                            float* key_t2 = inp + b*T*C3 + t2*C3 + h*hs + C;
+                                            float* dkey_t2 = dinp + b*T*C3 + t2*C3 + h*hs + C;
+                                            for (int i=0; i<hs; i++){
+                                                dquery_t[i] += key_t2[i]*dpreatt_bth[t2]*scale;
+                                                dkey_t2[i] += query_t[i]*dpreatt_bth[t2]*scale;
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+
+
+
+#define GELU_SCALING_FACTOR sqrtf(2.0f/M_PI)      //M_PI is pi
+
+void gelu_forward(float* out, float* inp, int N){
+    for (int i=0; i<N; i++){
+        float x = inp[i];
+        float cube = 0.044715f*x*x*x;
+        out[i] = 0.5f*x*(1.0f + tanhf(GELU_SCALING_FACTOR*(x + cube)));
+    }
+}
+
+
+
+
